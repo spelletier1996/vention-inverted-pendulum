ead010af9059f2edd1b551edff3924a7e10b4718
seb <spelletier1996@gmail.com>
2024-08-28 20:39:40 -0400

    model cleanup

--- simulation/include/inverted_pendulum.hpp
+++ simulation/include/inverted_pendulum.hpp
@@ -1,8 +1,77 @@
-#ifndef INCLUDE_INVERTED_PENDULUM_HPP
-#define INCLUDE_INVERTED_PENDULUM_HPP
+#ifndef SRC_INVERTED_PENDULUM_HPP
+#define SRC_INVERTED_PENDULUM_HPP
 
-//TEMP
+#include "Eigen/Dense"
+
 class InvertedPendulum {
+public:
+  //!
+  //!@brief Construct a new Inverted Pendulum object
+  //!
+  //!@param M base mass (kg)
+  //!@param m pendulum mass (kg)
+  //!@param J pendulum moment (kg m^2)
+  //!@param l pendulum length (m)
+  //!@param c base friction coefficient (N s/m)
+  //!@param gamma pendulum friction coefficient (N m s/rad)
+  //!@param x_0 Initial conditions (m, rad, m/s, rad/s)
+  //!
+  InvertedPendulum(double M, double m, double J, double l, double c,
+                   double gamma, Eigen::VectorXd x_0)
+      : M_(M), m_(m), J_(J), l_(l), c_(c), gamma_(gamma), g_(9.81), M_t_(M + m),
+        J_t_(J + m * std::pow(l, 2)), x_(x_0),
+        x_dot_(Eigen::VectorXd(4).setZero()) {}
+
+  //!
+   //!@brief Constructs with default values and given initial conditions
+   //!@param x_0 
+  //!
+  InvertedPendulum(Eigen::VectorXd x_0)
+      : InvertedPendulum(1.0, 1.0, 1.0, 1.0, 1.0, 1.0, x_0) {}
+
+  InvertedPendulum() : InvertedPendulum(Eigen::VectorXd(4)) {
+    x_ << 0, 0, 0, 0;
+  }
+
+  //!
+  //!@brief Updates the inverted pendulum sim
+  //!@param dt time step (seconds)
+  //!@param cart_force cart force (N)
+  //!@param pendulum_disturbance pendulum disturbance (N)
+  //!
+  void Update(const double &dt, const double &cart_force,
+              const double &pendulum_disturbance);
+
+  //!
+  //!@brief Returns current state vector
+  //!@return Eigen::VectorXd (position m, angle rad, velocity m/s, angular
+  //! velocity rad/s)
+  //!
+  [[nodiscard]] auto State() const -> Eigen::VectorXd { return x_; }
+
+  void Restart() {
+    x_.setZero();
+    Restart(x_);
+  }
+
+  void Restart(Eigen::VectorXd initial_conditions) {
+    x_ = initial_conditions;
+    x_dot_.setZero();
+  }
+
+private:
+  const double M_;     // mass of the base
+  const double m_;     // mass of the pendulum
+  const double J_;     // moment of inertia of the pendulum
+  const double l_;     // distance from the base to the pendulum
+  const double c_;     // coefficient of viscous friction (base)
+  const double gamma_; // coefficient of viscous friction (pendulum)
+  const double g_;     // acceleration due to gravity
+  const double M_t_;   // total mass
+  const double J_t_;   // total inertia
+
+  Eigen::VectorXd x_;     // state vector
+  Eigen::VectorXd x_dot_; // state vector derivative
 };
 
-#endif  // INCLUDE_INVERTED_PENDULUM_HPP
+#endif // SRC_INVERTED_PENDULUM_HPP
---
+++ simulation/src/inverted_pendulum.cpp
@@ -0,0 +1,33 @@
+#include "inverted_pendulum.hpp"
+
+void InvertedPendulum::Update(const double &dt, const double &cart_force,
+                              const double &pendulum_disturbance) {
+  // Recover state parameters
+  double x = x_(0);                // position of the base
+  double theta = x_(1);            // angle of the pendulum
+  double vx = x_(2);               // velocity of the base
+  double omega = x_(3);            // angular rate of the pendulum
+  double d = pendulum_disturbance; // disturbance TODO: add to params
+  double u = cart_force; // force applied to the base TODO: add to params
+
+  // Compute common terms
+  double s_t = std::sin(theta);
+  double c_t = std::cos(theta);
+  double o_2 = std::pow(omega, 2);
+  double l_2 = std::pow(l_, 2);
+
+  // Calculate derivatives
+  x_dot_(0) = vx;
+  x_dot_(1) = omega;
+  x_dot_(2) =
+      (-m_ * l_ * s_t * o_2 + m_ * g_ * (m_ * l_2 / J_t_) * s_t * c_t -
+       c_ * vx - (gamma_ / J_t_) * m_ * l_ * c_t * omega + u + d * c_t) /
+      (M_t_ - m_ * (m_ * l_2 / J_t_) * c_t * c_t);
+  x_dot_(3) =
+      (-m_ * l_2 * s_t * c_t * o_2 + M_t_ * g_ * l_ * s_t - c_ * l_ * c_t * vx -
+       gamma_ * (M_t_ / m_) * omega + l_ * c_t * u + d * s_t) /
+      (J_t_ * (M_t_ / m_) - m_ * (l_ * c_t) * (l_ * c_t));
+
+  // Apply Euler method to solve differential equations
+  x_ += x_dot_ * dt;
+}
